09083bef19f2012f9d167a2651637ef4d3ed89ec
kalaiR <kalaimca.gs@gmail.com>
2017-06-02 21:03:32 +1000

    update ip issues fixed

--- server.js
+++ server.js
@@ -10,43 +10,44 @@ app.get('/',function(req,res){
 app.use(express.static('assets')); //This folder contains the css and js files
 
 // app.listen(3000);
-var io = require('socket.io').listen(app.listen(3000,'192.168.0.9'));
+// var io = require('socket.io').listen(app.listen(3000,'192.168.0.9'));
+var io = require('socket.io').listen(app.listen(3000));
 
 console.log("Running at Port 3000");
 
 io.sockets.on('connection', function (socket) {
     socket.on('api_data', function (data) {
     	console.log(data);
-        url='http://192.168.0.18/api/callAction';
-    	if(data.purpose == "light_on_off"){
-    		// console.log("if");  		
-    		qs = { deviceID: data.deviceid, name: data.act };
-    	}
-    	else if(data.purpose == "light_dimmer"){
-    		// console.log("else if");
-    		qs = { deviceID: data.deviceid, name: data.act, arg1:data.range};
-    	}
-        else if(data.purpose == "tv_control"){
-            // console.log("tv_control");
-            qs = { deviceID: data.deviceid, name: data.act, arg1:data.button_id};
-        }
-        else if(data.purpose == "ac_control"){
-            // console.log("tv_control");
-            qs = { deviceID: data.deviceid, name: data.act, arg1:data.temp_val};
-        }
-        var options = { method: 'GET',
-		url: url,
-		qs: qs,
-		headers:
-	    { 'postman-token': 'bf759df7-714a-781f-f424-f1fc799ecc41',
-	     'cache-control': 'no-cache',
-	     authorization: 'Basic c2F0aGVlc2hAd2ViZXppLmNvbS5hdTpXZWJlemkxMiE=' } };
-        console.log(options); 
-		request(options, function (error, response, body) {
-		  if (error) throw new Error(error);
-          // console.log(response);
-		  // console.log(body);
-		});
+  //       url='http://192.168.0.18/api/callAction';
+  //   	if(data.purpose == "light_on_off"){
+  //   		// console.log("if");  		
+  //   		qs = { deviceID: data.deviceid, name: data.act };
+  //   	}
+  //   	else if(data.purpose == "light_dimmer"){
+  //   		// console.log("else if");
+  //   		qs = { deviceID: data.deviceid, name: data.act, arg1:data.range};
+  //   	}
+  //       else if(data.purpose == "tv_control"){
+  //           // console.log("tv_control");
+  //           qs = { deviceID: data.deviceid, name: data.act, arg1:data.button_id};
+  //       }
+  //       else if(data.purpose == "ac_control"){
+  //           // console.log("tv_control");
+  //           qs = { deviceID: data.deviceid, name: data.act, arg1:data.temp_val};
+  //       }
+  //       var options = { method: 'GET',
+		// url: url,
+		// qs: qs,
+		// headers:
+	 //    { 'postman-token': 'bf759df7-714a-781f-f424-f1fc799ecc41',
+	 //     'cache-control': 'no-cache',
+	 //     authorization: 'Basic c2F0aGVlc2hAd2ViZXppLmNvbS5hdTpXZWJlemkxMiE=' } };
+  //       // console.log(options); 
+		// request(options, function (error, response, body) {
+		//   if (error) throw new Error(error);
+  //         // console.log(response);
+		//   // console.log(body);
+		// });
     });
 	// console.log("User Connected");
 });
